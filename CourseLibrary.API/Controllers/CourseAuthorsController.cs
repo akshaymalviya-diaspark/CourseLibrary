c1efe81fa9ceece168c8dccc24afe39afc1280c4
Akshay_Malviya <malviyaakshay88@gmail.com>
2020-05-05 12:17:36 +0530

    Add project files.

---
+++ CourseLibrary.API/Controllers/CourseAuthorsController.cs
@@ -0,0 +1,35 @@
+﻿using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/courseauthors")]
+    public class CourseAuthorsController : ControllerBase
+    {
+        ICourseLibraryRepository _repository;
+
+        public CourseAuthorsController(ICourseLibraryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseAuthorDto>> Get(string courseName = "", string authorName = "")
+        {
+            /*
+            Author a = _repository.GetAuthors().First();
+            a.FirstName = a.FirstName + " - 1";
+            _repository.UpdateAuthor(a);
+            _repository.Save();
+            */
+            return new ActionResult<IEnumerable<CourseAuthorDto>>(_repository.GetCourseAuthors(courseName, authorName));
+        }
+    }
+}
---
+++ CourseLibrary.API/Models/CourseAuthorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    public class CourseAuthorDto
+    {
+        public string Course { get; set; }
+        public string Author { get; set; }
+    }
+}
